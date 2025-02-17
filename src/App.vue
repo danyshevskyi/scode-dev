cc1b301b2eaa1a07008ae6e0c085eb7289567e56
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-02-17 15:53:42 +0200

    Create components

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,114 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import PageAuth from './components/PageAuth.vue'
+import PageNotAuth from './components/PageNotAuth.vue'
+import ModalAbout from './components/modal/ModalAbout.vue'
+import ModalLogin from './components/modal/ModalAuth.vue'
+import axios from 'axios'
+
+
+
+// const token = "eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0"
+
+// const body = {
+//     "name": "Axios",
+//     "email": "axios@gmail.com",
+//     "password": "12345678"
+// }
+
+// const header = {
+//     "Accept": "application/json",
+//     "X-XSRF-TOKEN": token
+// }
+
+
+
+async function sentFetch() {
+    const sss = await fetch (
+                    'http://127.0.0.1:8000/api/reg',
+                    {
+                        method: 'POST',  
+                        headers:
+                                {
+                        'Accept': 'application/json',
+                        'X-XSRF-TOKEN':            'eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0'
+                                },
+                        body: JSON.stringify(      {  name: "Axios",
+        email: "axios4@gmail.com",
+        password: "12345678"
+                    }).then(sss.json())
+                    }
+                );
+                    // const result = await response.json();
+
+                    // return sss;
+}
+
+
+
+
+function sentData() {
+
+    return axios.post(
+    'http://127.0.0.1:8000/api/reg',
+    {
+        name: "Axios",
+        email: "axios4@gmail.com",
+        password: "12345678"
+    },
+    {
+    headers: {
+                        'Accept': 'application/json',
+                        'X-XSRF-TOKEN':            'eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0'
+                    }
+    }
+
+)
+.then(response => {
+    console.log(response.data);
+})
+.catch(error => {
+      console.error(error.response.data.message);
+    //   const qqq = ref(error.response.data.message);
+    // return 'a';
+})
+
+// return console.error(error.response.data.message);
+
+}
+
+
+
+console.log(sentFetch());
+
+// const auth = false;
+
+// if(sentData) {
+    
+// } else {
+//     const auth = ref(false)
+// }
+
+
+
+// console.log(sentData());
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+<PageAuth v-if="false"></PageAuth>
+<PageNotAuth v-else></PageNotAuth>
 
-  <main>
-    <TheWelcome />
-  </main>
+
+
+<div>{{ }}</div>
+
+<ModalAbout />
+<ModalLogin />
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/modal/ModalAbout.vue
@@ -0,0 +1,85 @@
+<script setup>
+</script>
+
+<template>
+
+<div class="modal fade" tabindex="-1" id="mod_about">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title"><i class="bi bi-info-circle ps-1 pe-2"></i>About</h5> 
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+  
+  <div class="mt-2 px-2">
+      
+  <div class="container col-auto t18">
+      
+    <p class="mt-1 mb-0" style="text-align: justify; text-indent: 10px;">
+      <span class="fw-medium"><i class="bi bi-copy pe-1"></i>English Cards</span> is a free web-application for creating
+        your own flashcards with words, phrases, sentences or dialogues.
+    </p>
+  
+    <p class="1mb-0 my-2 1bb" style="text-align: justify; text-indent: 25px;">
+      
+      <span class="fw-medium">For example</span>, when you want to learn the top English words - just record them on the
+        cards and they will always be with you on your smartphone or laptop.
+  
+    </p>
+  
+    <p class="mb-2" style="text-align: justify; text-indent: 25px;">
+      <span class="fw-medium">Everything is saved</span> in the cloud and accessible on any device at:
+    </p>
+  
+    <p class="text-center">
+        <a href="https://dov.pp.ua/english-cards" class="text-decoration-none">https://dov.pp.ua/scod</a> 
+    </p>
+  
+    <p class="b1b mb-0">
+      <span class="fw-medium"><i class="bi bi-check2-circle fs-5 ps-4"></i> Technology stack:</span>
+    </p>
+        <ul class="mb-1 1bb">
+            <li>HTML 5.0, CSS 3.0, JavaScript 3.0</li>
+            <li>Bootstrap v.5.3, jQuery 3.7.1</li>
+            <li>PHP 8.2 (MVC, Eloquent ORM, Twig)</li>
+            <li>MySQL</li>
+        </ul>
+    <p class="text-center mt-3"> 
+      <a href="https://github.com/danyshevskyi/english-cards" class="text-decoration-underline text-dark" target="_blank">
+          <i class="bi bi-github pe-1 1text-black"></i>English Cards on GitHub
+      </a>
+    </p>
+  
+    <p class="text-center mt-3"> 
+      <a href="https://dov.pp.ua" class="text-decoration-none text-dark" target="_blank">
+          More projects on <span class="text-decoration-none text-primary">https://dov.pp.ua</span>
+      </a>
+    </p>
+  </div>
+  
+  <div class="d-flex justify-content-center mt-4 mb-2">
+      <button type="button"
+              id="aboutButReg"
+              class="btn btn-outline-primary rounded-5 col-11 py-2 fs-5 1px-5 text-decoration-none"
+              style="margin-bottom: 13px;"
+              data-bs-toggle="modal"
+              data-bs-target="#mod_entry"
+              onclick="pressReg()">Registration</button>
+  </div>
+  
+  <a class="text-decoration-none text-black" href="https://www.linkedin.com/in/danyshevskyi/" target="_blank"> 
+      <div class="text-center 1mt-1 mb-4" style="font-size: 13px;">Oleksii Danyshevskyi &copy; 2024    
+      <i class="bi bi-linkedin"></i>
+    </div>
+  </a>
+  
+  </div>
+      
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
