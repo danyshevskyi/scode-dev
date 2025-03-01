da3d286a416405a630746f98015413da9fc88d70
Oleksii Danyshevskyi <danishevskij@gmail.com>
2025-03-01 19:45:05 +0200

    Function registration users

--- src/App.vue
+++ src/App.vue
@@ -6,92 +6,31 @@ import ModalAbout from './components/modal/ModalAbout.vue'
 import ModalLogin from './components/modal/ModalAuth.vue'
 import axios from 'axios'
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
-
-// const token = "eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0"
-
-// const body = {
-//     "name": "Axios",
-//     "email": "axios@gmail.com",
+// const credentials = {
+//     "name": "Alex",
+//     "email": "alex@gmail.com",
 //     "password": "12345678"
 // }
 
-// const header = {
-//     "Accept": "application/json",
-//     "X-XSRF-TOKEN": token
-// }
-
-
-
-async function sentFetch() {
-    const sss = await fetch (
-                    'http://127.0.0.1:8000/api/reg',
-                    {
-                        method: 'POST',  
-                        headers:
-                                {
-                        'Accept': 'application/json',
-                        'X-XSRF-TOKEN':            'eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0'
-                                },
-                        body: JSON.stringify(      {  name: "Axios",
-        email: "axios4@gmail.com",
-        password: "12345678"
-                    }).then(sss.json())
-                    }
-                );
-                    // const result = await response.json();
-
-                    // return sss;
+async function register() {
+   axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+      axios.post("http://localhost:8000/register",
+         {
+            name: "Alex4",
+            email: "alex4@gmail.com",
+            password: "12345678",
+            password_confirmation: "12345678"
+         }
+      ).catch(error => {
+         console.log(error)
+      })
+   })
 }
 
-
-
-
-function sentData() {
-
-    return axios.post(
-    'http://127.0.0.1:8000/api/reg',
-    {
-        name: "Axios",
-        email: "axios4@gmail.com",
-        password: "12345678"
-    },
-    {
-    headers: {
-                        'Accept': 'application/json',
-                        'X-XSRF-TOKEN':            'eyJpdiI6Inh6bFZKTkNQWXc4YUJTd3Vua3BWV2c9PSIsInZhbHVlIjoidjgxT2pVOFpkQ08vdjBzL24xNXJBb3B4Mm8rTWFweEpERHBBSGZVWEJDMzZEQllkRzhVRTFrSlhWUFVnQnd1SEo0RXNUZlBuM21NczdIdHpXdzlQYzN1cGRZTHhlcVNEdndnQmo4QVVCTGxScW1tcktFaDZ2MElxVnE1UDdDVVQiLCJtYWMiOiJiYmI0ZmMyN2FmMjFjYzI0ZjM2ZDE5NGZmYjgwNDAxMTkwOTBhNDRkZDgzMWJjZmVjZmRiZGJlODFhZTI5NTk3IiwidGFnIjoiIn0'
-                    }
-    }
-
-)
-.then(response => {
-    console.log(response.data);
-})
-.catch(error => {
-      console.error(error.response.data.message);
-    //   const qqq = ref(error.response.data.message);
-    // return 'a';
-})
-
-// return console.error(error.response.data.message);
-
-}
-
-
-
-console.log(sentFetch());
-
-// const auth = false;
-
-// if(sentData) {
-    
-// } else {
-//     const auth = ref(false)
-// }
-
-
-
-// console.log(sentData());
+register();
 
 </script>
 
